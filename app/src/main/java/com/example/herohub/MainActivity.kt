fc6ce701f7090060afb31c0e13f949b83734dea9
Mohamed Ragab <mohamed.ragab.ali.9@gmail.com>
2023-05-02 19:41:21 +0300

    implement navigation ; implement bottom navigation

--- app/src/main/java/com/example/herohub/MainActivity.kt
+++ app/src/main/java/com/example/herohub/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.herohub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+//        val navController = findNavController(R.id.fragment_host)
+//        NavigationUI.setupWithNavController(bottomNavigationView , navController)
     }
 }
