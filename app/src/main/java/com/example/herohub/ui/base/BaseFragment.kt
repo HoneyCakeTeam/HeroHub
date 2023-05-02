2fc4515303f5785e2b73bd20870cdf911156eb7f
Haidy <haidyabugom3a@gmail.com>
2023-05-02 07:37:39 +0300

    add base fragment

---
+++ app/src/main/java/com/example/herohub/ui/base/BaseFragment.kt
@@ -0,0 +1,42 @@
+package com.example.herohub.ui.base
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+
+    abstract val TAG: String
+    abstract val bindingInflater: (LayoutInflater) -> VB
+    private var _binding: VB? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setup()
+    }
+
+    abstract fun setup()
+
+    protected fun log(value: Any) {
+        Log.e(TAG, value.toString())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
