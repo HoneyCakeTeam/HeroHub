16814b191c33285396352dc9f0cfcf1de4025b86
Nour Elden Alaa <nourelden515@gmail.com>
2023-05-02 20:58:25 +0300

    add state wrapper

--- app/src/main/java/com/example/herohub/Repository.kt
+++ app/src/main/java/com/example/herohub/Repository.kt
@@ -1,11 +1,20 @@
 package com.example.herohub
 
-import com.example.herohub.model.Character
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
 
 class Repository {
-    fun getAllCharacters(): Single<Response<Character>> {
-        return MarvelApi.marvelService.getAllCharacters()
+    fun getAllCharacters() = wrapWithState {
+        MarvelApi.marvelService.getAllCharacters()
+    }
+
+    private fun <T> wrapWithState(function: () -> Single<Response<T>>): Single<State<T>> {
+        return function().map {
+            if (it.isSuccessful) {
+                State.Success(it.body())
+            } else {
+                State.Failure(it.message())
+            }
+        }
     }
 }
