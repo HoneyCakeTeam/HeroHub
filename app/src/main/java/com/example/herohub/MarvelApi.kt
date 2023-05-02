999c1a168a800110a53c2a01811c6f9a07a9438a
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-02 13:50:09 +0300

    refactor MarvelApi.kt name

--- app/src/main/java/com/example/herohub/MarvelApi.kt
+++ app/src/main/java/com/example/herohub/MarvelApi.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RemoteDataSource {
+object MarvelApi {
     private const val BASE_URL = "http://gateway.marvel.com/v1/public/"
     private val logInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BASIC
