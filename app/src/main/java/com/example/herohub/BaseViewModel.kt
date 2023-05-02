b77181108b687abf6f5122dbd25fcb9a179c8a02
hamsa <hamsaalsaadie@gmail.com>
2023-05-02 09:02:23 +0300

    change onCleared function

--- app/src/main/java/com/example/herohub/BaseViewModel.kt
+++ app/src/main/java/com/example/herohub/BaseViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.herohub
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 
@@ -8,7 +9,7 @@ class BaseViewModel : ViewModel() {
     private val compositeDisposable: CompositeDisposable by lazy { CompositeDisposable() }
 
     override fun onCleared() {
-
+        Log.i("BaseViewModel", "on clear")
         compositeDisposable.dispose()
         super.onCleared()
     }
