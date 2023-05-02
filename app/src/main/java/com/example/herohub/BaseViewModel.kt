a29e393732284e0e97efa9278f715ed9f7e1d3b8
hamsa <hamsaalsaadie@gmail.com>
2023-05-02 17:53:46 +0300

    change addToCompositeDisposable

--- app/src/main/java/com/example/herohub/BaseViewModel.kt
+++ app/src/main/java/com/example/herohub/BaseViewModel.kt
@@ -10,15 +10,16 @@ abstract class BaseViewModel : ViewModel() {
     private val compositeDisposable: CompositeDisposable by lazy { CompositeDisposable() }
     abstract val TAG: String
 
+    protected fun addToCompositeDisposable(disposable: Disposable) {
+        compositeDisposable.add(disposable)
+    }
     fun CompositeDisposable.addToCompositeDisposable(disposable: Disposable) {
         this.add(disposable)
     }
-
     override fun onCleared() {
         super.onCleared()
         compositeDisposable.clear()
     }
-
     protected open fun log(message: String) {
         Log.v(TAG, message)
     }
