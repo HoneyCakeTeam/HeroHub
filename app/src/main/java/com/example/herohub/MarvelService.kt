9d54ac70888008254d3228fabaa4645c8199d723
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-05-02 17:25:06 +0300

    add getAllCharacters to get characters

--- app/src/main/java/com/example/herohub/MarvelService.kt
+++ app/src/main/java/com/example/herohub/MarvelService.kt
@@ -1,10 +1,11 @@
 package com.example.herohub
 
+import com.example.herohub.model.Character
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface MarvelService {
     @GET("characters")
-    fun getAllCharacters() : Single<Response<CharacterResponse>>
+    fun getAllCharacters() : Single<Response<Character>>
 }
