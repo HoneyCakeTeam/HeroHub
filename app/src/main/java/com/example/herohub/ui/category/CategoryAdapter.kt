1b0f8c1f85691c60b39de215b0f6902786ae4735
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-03 20:37:21 +0300

    remove unnecessary images

--- app/src/main/java/com/example/herohub/ui/category/CategoryAdapter.kt
+++ app/src/main/java/com/example/herohub/ui/category/CategoryAdapter.kt
@@ -1,12 +1,11 @@
 package com.example.herohub.ui.category
 
 import com.example.herohub.R
-import com.example.herohub.model.Character
 import com.example.herohub.ui.base.BaseAdapter
 import com.example.herohub.ui.base.BaseInteractionListener
 
-class CategoryAdapter(category: List<Character>, listener: CategoryInteractionListener) :
-    BaseAdapter<Character>(category, listener) {
+class CategoryAdapter(category: List<String>, listener: CategoryInteractionListener) :
+    BaseAdapter<String>(category, listener) {
     override val layoutId: Int = R.layout.item_category
 }
 
