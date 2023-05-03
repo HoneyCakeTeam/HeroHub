bdff7eacbb95897fe9f53397424740301adccf5f
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-05-03 17:39:02 +0300

    implement DiffUtils

--- app/src/main/java/com/example/herohub/ui/search/DiffUtils.kt
+++ app/src/main/java/com/example/herohub/ui/search/DiffUtils.kt
@@ -1,4 +1,17 @@
 package com.example.herohub.ui.search
 
-class DiffUtils {
+import androidx.recyclerview.widget.DiffUtil
+import com.example.herohub.model.Character
+
+class DiffUtils(private val OldList: List<Character>, private val NewList: List<Character>) :
+    DiffUtil.Callback() {
+    override fun getOldListSize() = OldList.size
+    override fun getNewListSize() = NewList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return (OldList[oldItemPosition].id == NewList[newItemPosition].id
+                || OldList[oldItemPosition].name == NewList[newItemPosition].name)
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) = true
 }
