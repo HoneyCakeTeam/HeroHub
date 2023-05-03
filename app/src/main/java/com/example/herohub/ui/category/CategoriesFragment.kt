f72dc66ea51d5d7bee8f6e07fced5c655364dfbe
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-03 14:21:53 +0300

    create destination fragments in CategoriesFragment

--- app/src/main/java/com/example/herohub/ui/category/CategoriesFragment.kt
+++ app/src/main/java/com/example/herohub/ui/category/CategoriesFragment.kt
@@ -1,6 +1,8 @@
 package com.example.herohub.ui.category
 
 import android.view.LayoutInflater
+import androidx.navigation.Navigation
+import com.example.herohub.data.local.model.Category
 import com.example.herohub.databinding.FragmentCategoriesBinding
 import com.example.herohub.ui.base.BaseFragment
 
@@ -15,6 +17,17 @@ class CategoriesFragment : BaseFragment<FragmentCategoriesBinding , CategoryView
     override fun setup() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+
+        initCategoryAdapter()
+        viewModel.itemCategory.observe(this){category ->
+            Navigation.findNavController(binding.root).navigate()
+        }
     }
+
+    private fun initCategoryAdapter() {
+        val adapter = CategoryAdapter(emptyList() , viewModel)
+        binding.categoriesRecycler.adapter = adapter
+    }
+
 
 }
--- app/src/main/java/com/example/herohub/ui/category/CategoryViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/category/CategoryViewModel.kt
@@ -1,8 +1,17 @@
 package com.example.herohub.ui.category
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.herohub.data.local.model.Category
 import com.example.herohub.ui.base.BaseViewModel
 
-class CategoryViewModel:BaseViewModel(){
+class CategoryViewModel:BaseViewModel() , CategoryInteractionListener{
     override val TAG: String = this::class.java.simpleName
+
+    private val _itemCategory = MutableLiveData<Category>()
+    val itemCategory:LiveData<Category> = _itemCategory
+    override fun onCategoryClick(item: Category) {
+        _itemCategory.postValue(item)
+    }
 
 }
