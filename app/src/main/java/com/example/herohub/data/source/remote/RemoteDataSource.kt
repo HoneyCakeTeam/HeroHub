df52fb0397c4c1c9a5061b5c3ce322f0259d35a1
Mohamed Naser <mahmmedn19@gmail.com>
2023-05-03 19:41:53 +0300

    get all series

--- app/src/main/java/com/example/herohub/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/example/herohub/data/source/remote/RemoteDataSource.kt
@@ -2,4 +2,5 @@ package com.example.herohub.data.source.remote
 
 class RemoteDataSource {
     fun getAllCharacters() = MarvelApi.marvelService.getAllCharacters()
+    fun getAllSeries() = MarvelApi.marvelService.getAllSeries()
 }
