3f4745e155b891aecb78e998b5238f4658a70232
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-03 13:21:01 +0300

    implement getAllCharacters() to observe on live data of Character model

--- app/src/main/java/com/example/herohub/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/home/HomeViewModel.kt
@@ -1,19 +1,43 @@
 package com.example.herohub.ui.home
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.herohub.data.Repository
+import com.example.herohub.model.Character
 import com.example.herohub.ui.base.BaseViewModel
+import com.example.herohub.utills.State
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class HomeViewModel : BaseViewModel() {
-
     override val TAG: String
         get() = this::class.java.simpleName.toString()
 
-    override fun log(message: String) {
-        super.log(message)
-        Log.e(TAG, message)
+    private val repository: Repository by lazy { Repository() }
+
+    private val _characterResponse = MutableLiveData<State<Character>>()
+    val characterResponse: LiveData<State<Character>>
+        get() = _characterResponse
+
+    init {
+        getAllCharacters()
     }
 
-    override fun onCleared() {
-        super.onCleared()
+    private fun getAllCharacters() {
+        _characterResponse.postValue(State.Loading)
+
+        repository.getAllCharacters()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(::onGetCharacterSuccess, ::onGetCharacterError)
+            .addToCompositeDisposable()
+    }
+
+    private fun onGetCharacterSuccess(state: State<Character>) =
+        _characterResponse.postValue(state)
+
+    private fun onGetCharacterError(errorMessage: Throwable) {
+        _characterResponse.postValue(State.Error(errorMessage.message.toString()))
+        log(errorMessage.message.toString())
     }
 }
