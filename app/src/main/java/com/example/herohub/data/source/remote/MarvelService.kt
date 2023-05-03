d5cf629c25dc0c397d09e8259bea7a64fdb7f3a1
Haidy <haidyabugom3a@gmail.com>
2023-05-03 20:00:03 +0300

    get characters data in search view model

--- app/src/main/java/com/example/herohub/data/source/remote/MarvelService.kt
+++ app/src/main/java/com/example/herohub/data/source/remote/MarvelService.kt
@@ -1,5 +1,6 @@
 package com.example.herohub.data.source.remote
 
+import com.example.herohub.model.BaseResponse
 import com.example.herohub.model.Character
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
@@ -7,5 +8,5 @@ import retrofit2.http.GET
 
 interface MarvelService {
     @GET("characters")
-    fun getAllCharacters() : Single<Response<Character>>
+    fun getAllCharacters() : Single<Response<BaseResponse<Character>?>>
 }
