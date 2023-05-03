1ccf4b4162d181d82c5c89308e3dca0782c7c4f4
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-03 12:15:41 +0300

    prepare CategoriesFragment

--- app/src/main/java/com/example/herohub/ui/category/CategoriesFragment.kt
+++ app/src/main/java/com/example/herohub/ui/category/CategoriesFragment.kt
@@ -1,19 +1,21 @@
 package com.example.herohub.ui.category
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.herohub.R
+import com.example.herohub.databinding.FragmentCategoriesBinding
+import com.example.herohub.ui.base.BaseFragment
 
-class CategoriesFragment : Fragment() {
+class CategoriesFragment : BaseFragment<FragmentCategoriesBinding , CategoryViewModel >() {
+    override val TAG: String = this::class.java.simpleName
+    override val bindingInflater: (LayoutInflater) -> FragmentCategoriesBinding
+        get() = FragmentCategoriesBinding::inflate
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false)
+    override fun getViewModel() = CategoryViewModel::class.java
+
+
+    override fun setup() {
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
     }
+
 }
