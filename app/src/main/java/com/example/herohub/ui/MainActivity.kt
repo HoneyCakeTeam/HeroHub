82f41f00e4e57c764addff4be195969b2c90cfb2
Mohamed Ragab <mohamed.ragab.ali.9@gmail.com>
2023-05-03 12:47:54 +0300

    refactor navigation code

--- app/src/main/java/com/example/herohub/ui/MainActivity.kt
+++ app/src/main/java/com/example/herohub/ui/MainActivity.kt
@@ -4,34 +4,30 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.herohub.R
 import com.example.herohub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
+    private lateinit var  navController :NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_host) as NavHostFragment
-//        navController = navHostFragment.navController
-//        NavigationUI.setupWithNavController(binding.bottomNav, navController)
-//        NavigationUI.setupActionBarWithNavController(this , navController )
     }
-
-//    override fun onSupportNavigateUp(): Boolean {
-//        findNavController(R.id.fragment_host).navigateUp()
-//        return true
-//    }
-
 
     override fun onResume() {
         super.onResume()
-        navController = findNavController(R.id.fragment_host)
+        navController  = findNavController(R.id.fragment_host)
+        NavigationUI.setupActionBarWithNavController(this , navController )
         binding.bottomNav.setupWithNavController(navController)
-//        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return true
     }
 }
