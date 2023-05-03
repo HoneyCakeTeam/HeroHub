8bd03b92dde27621057e3fc44e3f1a2e139b64ba
Haidy <haidyabugom3a@gmail.com>
2023-05-03 17:56:30 +0300

    implement listener in view search view model

--- app/src/main/java/com/example/herohub/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/search/SearchViewModel.kt
@@ -3,10 +3,12 @@ package com.example.herohub.ui.search
 import com.example.herohub.data.Repository
 import com.example.herohub.ui.base.BaseViewModel
 
-class SearchViewModel : BaseViewModel() {
+class SearchViewModel : BaseViewModel(), SearchInteractionListener {
     override val TAG: String = this::class.java.simpleName.toString()
     private val repository = Repository()
-
+    override fun <T> onItemClick(item: T) {
+        TODO("Not yet implemented")
+    }
 
 
 }
