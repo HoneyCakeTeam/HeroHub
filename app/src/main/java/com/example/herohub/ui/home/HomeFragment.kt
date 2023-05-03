865b3c62ba37ffbdb075bca533e4f6f3a0f9d8ec
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-03 15:31:09 +0300

    update HomeFragment code

--- app/src/main/java/com/example/herohub/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/herohub/ui/home/HomeFragment.kt
@@ -1,19 +1,17 @@
 package com.example.herohub.ui.home
 
-import android.view.LayoutInflater
+import androidx.fragment.app.viewModels
+import com.example.herohub.R
 import com.example.herohub.databinding.FragmentHomeBinding
 import com.example.herohub.ui.base.BaseFragment
 
-class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override val TAG: String = this::class.java.simpleName.toString()
-    override val bindingInflater: (LayoutInflater) -> FragmentHomeBinding =
-        FragmentHomeBinding::inflate
-
-    override fun getViewModel(): Class<HomeViewModel> = HomeViewModel::class.java
+    override val layoutIdFragment: Int = R.layout.fragment_home
+    override val viewModel: HomeViewModel by viewModels()
 
     override fun setup() {
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
+
     }
 }
