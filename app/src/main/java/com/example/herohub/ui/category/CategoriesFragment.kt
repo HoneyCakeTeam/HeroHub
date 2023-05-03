741a87ae3c90828295c400503ec29f38de43fdd7
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-03 14:25:51 +0300

    create Extensions.kt

--- app/src/main/java/com/example/herohub/ui/category/CategoriesFragment.kt
+++ app/src/main/java/com/example/herohub/ui/category/CategoriesFragment.kt
@@ -2,11 +2,12 @@ package com.example.herohub.ui.category
 
 import android.view.LayoutInflater
 import androidx.navigation.Navigation
+import com.example.herohub.R
 import com.example.herohub.data.local.model.Category
 import com.example.herohub.databinding.FragmentCategoriesBinding
 import com.example.herohub.ui.base.BaseFragment
 
-class CategoriesFragment : BaseFragment<FragmentCategoriesBinding , CategoryViewModel >() {
+class CategoriesFragment : BaseFragment<FragmentCategoriesBinding, CategoryViewModel>() {
     override val TAG: String = this::class.java.simpleName
     override val bindingInflater: (LayoutInflater) -> FragmentCategoriesBinding
         get() = FragmentCategoriesBinding::inflate
@@ -19,13 +20,19 @@ class CategoriesFragment : BaseFragment<FragmentCategoriesBinding , CategoryView
         binding.lifecycleOwner = this
 
         initCategoryAdapter()
-        viewModel.itemCategory.observe(this){category ->
-            Navigation.findNavController(binding.root).navigate()
+        viewModel.itemCategory.observe(this) { category ->
+            when (category.categoryName) {
+                "Characters" -> {
+                    Navigation.findNavController(binding.root)
+                        .navigate(R.id.action_categories_fragment_to_characterFragment)
+                }
+            }
+
         }
     }
 
     private fun initCategoryAdapter() {
-        val adapter = CategoryAdapter(emptyList() , viewModel)
+        val adapter = CategoryAdapter(emptyList(), viewModel)
         binding.categoriesRecycler.adapter = adapter
     }
 
