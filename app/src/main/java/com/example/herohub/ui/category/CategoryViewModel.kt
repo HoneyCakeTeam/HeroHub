7830544f06754133ea23c958853352d57a1fac26
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-03 12:15:20 +0300

    create CategoryViewModel

---
+++ app/src/main/java/com/example/herohub/ui/category/CategoryViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.herohub.ui.category
+
+import com.example.herohub.ui.base.BaseViewModel
+
+class CategoryViewModel:BaseViewModel(){
+    override val TAG: String = this::class.java.simpleName
+}
