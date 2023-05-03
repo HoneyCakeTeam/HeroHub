50fba744fb7f5a54e737347559230f2a01348f51
Haidy <haidyabugom3a@gmail.com>
2023-05-03 19:17:48 +0300

    update search view model

--- app/src/main/java/com/example/herohub/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/example/herohub/ui/search/SearchAdapter.kt
@@ -4,7 +4,7 @@ import com.example.herohub.BaseAdapter
 import com.example.herohub.BaseInteractionListener
 import com.example.herohub.R
 
-class CategoryAdapter<T>(listener: SearchInteractionListener) :
+class SearchAdapter<T>(listener: SearchInteractionListener) :
     BaseAdapter<T>(listener) {
     override val layoutId: Int = R.layout.item_search
 }
--- app/src/main/java/com/example/herohub/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/search/SearchViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.herohub.ui.search
 
 import com.example.herohub.data.Repository
+import com.example.herohub.model.Character
 import com.example.herohub.ui.base.BaseViewModel
 
 class SearchViewModel : BaseViewModel(), SearchInteractionListener {
     override val TAG: String = this::class.java.simpleName.toString()
     private val repository = Repository()
+    val adapter = SearchAdapter<Character>(this)
+
+    fun getAllCharacter() = repository.getAllCharacters()
+
     override fun <T> onItemClick(item: T) {
         TODO("Not yet implemented")
     }
