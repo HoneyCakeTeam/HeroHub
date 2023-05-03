eafac557ca62947c961c8cd41ae696b34daee274
Nour Elden Alaa <nourelden515@gmail.com>
2023-05-03 18:34:02 +0300

    get event by id

--- app/src/main/java/com/example/herohub/ui/eventdetails/EventDetailsFragment.kt
+++ app/src/main/java/com/example/herohub/ui/eventdetails/EventDetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.herohub.ui.eventdetails
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.herohub.R
 import com.example.herohub.databinding.FragmentEventsDetailsBinding
 import com.example.herohub.ui.base.BaseFragment
@@ -9,7 +10,8 @@ class EventDetailsFragment : BaseFragment<FragmentEventsDetailsBinding>() {
     override val TAG: String = this::class.java.simpleName
     override val viewModel: EventDetailsViewModel by viewModels()
     override val layoutIdFragment = R.layout.fragment_events_details
+    private val arguments: EventDetailsFragmentArgs by navArgs()
     override fun setup() {
-        TODO("Not yet implemented")
+        viewModel.getEvent(arguments.eventId)
     }
 }
