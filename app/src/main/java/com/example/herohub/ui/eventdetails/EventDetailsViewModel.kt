15087482344577748570f10f6eed0fba336c5318
Nour Elden Alaa <nourelden515@gmail.com>
2023-05-03 15:12:05 +0300

    refactor base fragment

---
+++ app/src/main/java/com/example/herohub/ui/eventdetails/EventDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.herohub.ui.eventdetails
+
+import com.example.herohub.ui.base.BaseViewModel
+
+class EventDetailsViewModel : BaseViewModel() {
+    override val TAG = "EVENT_DETAILS_VIEW_MODEL"
+}
