d22d4257fc159ed18479f705486afefdc06d8099
hamsa <hamsaalsaadie@gmail.com>
2023-05-03 19:37:02 +0300

    fix visible and gone issues in bindingAdapters.kt

--- app/src/main/java/com/example/herohub/utills/bindingAdapters.kt
+++ app/src/main/java/com/example/herohub/utills/bindingAdapters.kt
@@ -10,19 +10,19 @@ import com.example.herohub.BaseAdapter
 @BindingAdapter("app:showWhenLoading")
 fun <T> showWhenLoading(view: View, state: State<T>?) {
     if (state is State.Loading) view.visibility = View.VISIBLE
-    else view.visibility = View.VISIBLE
+    else view.visibility = View.GONE
 }
 
 @BindingAdapter("app:showWhenError")
 fun <T> showWhenError(view: View, state: State<T>?) {
     if (state is State.Error) view.visibility = View.VISIBLE
-    else view.visibility = View.VISIBLE
+    else view.visibility = View.GONE
 }
 
 @BindingAdapter("app:showWhenSuccess")
 fun <T> showWhenSuccess(view: View, state: State<T>?) {
     if (state is State.Success) view.visibility = View.VISIBLE
-    else view.visibility = View.VISIBLE
+    else view.visibility = View.GONE
 }
 
 @BindingAdapter(value = ["app:items"])
