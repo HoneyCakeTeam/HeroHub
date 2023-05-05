403b762c36aff27441240238009e597ab2e49b2a
Mohamed Naser <mahmmedn19@gmail.com>
2023-05-06 00:05:38 +0300

    init adapter

--- app/src/main/java/com/example/herohub/ui/category/series/SeriesFragment.kt
+++ app/src/main/java/com/example/herohub/ui/category/series/SeriesFragment.kt
@@ -1,8 +1,25 @@
 package com.example.herohub.ui.category.series
 
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.herohub.R
+import com.example.herohub.databinding.FragmentSeriesBinding
+import com.example.herohub.ui.base.BaseFragment
 
 
-class SeriesFragment : Fragment() {
+class SeriesFragment : BaseFragment<FragmentSeriesBinding>() {
+
+    override val TAG: String = this::class.java.simpleName
+    override val layoutIdFragment: Int = R.layout.fragment_series
+    override val viewModel: SeriesViewModel by viewModels()
+
+    override fun setup() {
+        initiateAdapter()
+    }
+
+    private fun initiateAdapter() {
+        val adapter = SeriesAdapter(viewModel)
+
+
+    }
 
 }
