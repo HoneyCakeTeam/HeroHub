cbbe578b9c068f1364d72848cc73c2166ebb7b80
Mohamed Naser <mahmmedn19@gmail.com>
2023-05-06 00:15:54 +0300

    change listener

--- app/src/main/java/com/example/herohub/ui/category/series/SeriesAdapter.kt
+++ app/src/main/java/com/example/herohub/ui/category/series/SeriesAdapter.kt
@@ -3,12 +3,11 @@ package com.example.herohub.ui.category.series
 import com.example.herohub.R
 import com.example.herohub.model.Series
 import com.example.herohub.ui.base.BaseAdapter
-import com.example.herohub.ui.base.BaseInteractionListener
+import com.example.herohub.ui.base.BaseCategoryInteractionListener
 
-class SeriesAdapter(listener: SeriesInteractionListener) :
-    BaseAdapter<Series>(listener) {
+class SeriesAdapter(listener: SeriesInteractionListener) : BaseAdapter<Series>(listener) {
     override val layoutId: Int
         get() = R.layout.item_category
 }
 
-interface SeriesInteractionListener : BaseInteractionListener
+interface SeriesInteractionListener : BaseCategoryInteractionListener
