c0e19c1b9f2a683987bdca93a7497a8336059eda
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-05 09:53:58 +0300

    implement CategoryViewPagerAdapter

--- app/src/main/java/com/example/herohub/ui/category/CategoryViewPagerAdapter.kt
+++ app/src/main/java/com/example/herohub/ui/category/CategoryViewPagerAdapter.kt
@@ -1,4 +1,13 @@
 package com.example.herohub.ui.category
 
-class CategoryViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class CategoryViewPagerAdapter(container: Fragment, private val fragmentList: List<Fragment>) :
+    FragmentStateAdapter(container) {
+    override fun getItemCount(): Int = fragmentList.size
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
 }
