b461ae47345ce9128b01f107720c21e17193583e
Haidy <haidyabugom3a@gmail.com>
2023-05-05 23:50:34 +0300

    update comics fragment

--- app/src/main/java/com/example/herohub/ui/category/comics/ComicsFragment.kt
+++ app/src/main/java/com/example/herohub/ui/category/comics/ComicsFragment.kt
@@ -1,8 +1,20 @@
 package com.example.herohub.ui.category.comics
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import com.example.herohub.ui.base.BaseFragment
 
 
-class ComicsFragment : Fragment() {
+class ComicsFragment : BaseFragment<FragmentComicsBinding>() {
+    override val TAG: String
+        get() = TODO("Not yet implemented")
+    override val layoutIdFragment: Int
+        get() = TODO("Not yet implemented")
+    override val viewModel: ViewModel
+        get() = TODO("Not yet implemented")
+
+    override fun setup() {
+        TODO("Not yet implemented")
+    }
 
 }
