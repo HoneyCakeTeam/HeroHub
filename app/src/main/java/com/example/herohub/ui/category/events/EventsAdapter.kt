2915721a75735afb8893543086e9e9780c388cad
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-05-06 01:00:32 +0300

    implement EventsAdapter

--- app/src/main/java/com/example/herohub/ui/category/events/EventsAdapter.kt
+++ app/src/main/java/com/example/herohub/ui/category/events/EventsAdapter.kt
@@ -1,4 +1,12 @@
 package com.example.herohub.ui.category.events
 
-class EventsAdapter {
+import com.example.herohub.R
+import com.example.herohub.model.Event
+import com.example.herohub.ui.base.BaseAdapter
+import com.example.herohub.ui.base.BaseInteractionListener
+
+class EventsAdapter(listener: EventsInteractionListener):BaseAdapter<Event>(listener) {
+    override val layoutId: Int = R.layout.item_category
 }
+
+interface EventsInteractionListener : BaseInteractionListener
