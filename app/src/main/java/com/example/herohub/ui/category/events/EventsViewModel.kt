86714d98383690d509bc8f522744087fe3c71b15
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-05-06 01:45:59 +0300

    implement EventsViewModel.kt

--- app/src/main/java/com/example/herohub/ui/category/events/EventsViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/category/events/EventsViewModel.kt
@@ -1,4 +1,36 @@
 package com.example.herohub.ui.category.events
 
-class EventsViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.herohub.data.Repository
+import com.example.herohub.model.DataResponse
+import com.example.herohub.model.Event
+import com.example.herohub.ui.base.BaseViewModel
+import com.example.herohub.utills.UiState
+
+class EventsViewModel : BaseViewModel(), EventsInteractionListener {
+    override val TAG: String = this::class.java.simpleName
+
+    private val repository = Repository()
+
+    private val _events = MutableLiveData<UiState<DataResponse<Event>>>()
+    val events: LiveData<UiState<DataResponse<Event>>>
+        get() = _events
+
+    init {
+        getAllEvents()
+    }
+
+    private fun getAllEvents() {
+        disposeObservable(repository.getAllEvents(), ::onGetEventsSuccess, ::onError)
+    }
+
+    private fun onGetEventsSuccess(state: UiState<DataResponse<Event>>) {
+        _events.postValue(state)
+    }
+
+    private fun onError(throwable: Throwable) {
+        log("Error : ${throwable.message.toString()}")
+    }
+
 }
