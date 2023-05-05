bea231983915f5f0fc35d02e85625ec620b9de8f
Mohamed Naser <mahmmedn19@gmail.com>
2023-05-06 00:16:20 +0300

    add onClick

--- app/src/main/java/com/example/herohub/ui/category/series/SeriesViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/category/series/SeriesViewModel.kt
@@ -8,28 +8,33 @@ import com.example.herohub.model.Series
 import com.example.herohub.ui.base.BaseViewModel
 import com.example.herohub.utills.UiState
 
-class SeriesViewModel:BaseViewModel() , SeriesInteractionListener {
+class SeriesViewModel : BaseViewModel(), SeriesInteractionListener {
     override val TAG: String
         get() = this::class.java.simpleName
     private val repository = Repository()
     private val _allSeries = MutableLiveData<UiState<DataResponse<Series>>>()
 
-    val allSeries:LiveData<UiState<DataResponse<Series>>>
-            get() = _allSeries
+    val allSeries: LiveData<UiState<DataResponse<Series>>>
+        get() = _allSeries
 
     init {
         getAllSeries()
     }
 
     private fun getAllSeries() {
-        disposeObservable(repository.getAllSeries(),::onGetSeriesSuccess,::onError)
+        disposeObservable(repository.getAllSeries(), ::onGetSeriesSuccess, ::onError)
     }
 
-    private fun onGetSeriesSuccess(state :UiState<DataResponse<Series>>){
+    private fun onGetSeriesSuccess(state: UiState<DataResponse<Series>>) {
         _allSeries.postValue(state)
     }
 
-    private fun onError(errorMessage: Throwable){
+    private fun onError(errorMessage: Throwable) {
         _allSeries.postValue(UiState.Error(errorMessage.message.toString()))
     }
+
+    override fun onClickItem(id: Int) {
+        log("All Series ${id}")
+    }
+
 }
