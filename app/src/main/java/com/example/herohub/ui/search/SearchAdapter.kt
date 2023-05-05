324be26ed9d7e4984188aed4056e3204006f0bd5
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-05-06 00:15:18 +0300

    create adapter

--- app/src/main/java/com/example/herohub/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/example/herohub/ui/search/SearchAdapter.kt
@@ -1,16 +1,15 @@
 package com.example.herohub.ui.search
 
-
 import com.example.herohub.R
-import com.example.herohub.model.Character
 import com.example.herohub.ui.base.BaseAdapter
 import com.example.herohub.ui.base.BaseInteractionListener
 
-class SearchAdapter(listener: SearchInteractionListener) :
-    BaseAdapter<Character>(listener) {
-    override val layoutId: Int = R.layout.item_search
-}
+class SearchAdapter(listener: SearchInteractionListener)
+    : BaseAdapter<Character>(listener) {
+    override val layoutId: Int
+        get() = R.layout.item_search
 
-interface SearchInteractionListener : BaseInteractionListener {
-    fun <T> onItemClick(item: T)
+    interface SearchInteractionListener : BaseInteractionListener {
+        fun <T> onClickItem(item: T)
+    }
 }
