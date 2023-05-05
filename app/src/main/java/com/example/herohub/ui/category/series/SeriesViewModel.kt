cf4a65fd1e1ca6bedea21e022e006465100dcdb6
Mohamed Naser <mahmmedn19@gmail.com>
2023-05-06 00:05:01 +0300

    create series category viewModel

---
+++ app/src/main/java/com/example/herohub/ui/category/series/SeriesViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.herohub.ui.category.series
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.herohub.data.Repository
+import com.example.herohub.model.DataResponse
+import com.example.herohub.model.Series
+import com.example.herohub.ui.base.BaseViewModel
+import com.example.herohub.utills.UiState
+
+class SeriesViewModel:BaseViewModel() , SeriesInteractionListener {
+    override val TAG: String
+        get() = this::class.java.simpleName
+    private val repository = Repository()
+    private val _allSeries = MutableLiveData<UiState<DataResponse<Series>>>()
+
+    val allSeries:LiveData<UiState<DataResponse<Series>>>
+            get() = _allSeries
+
+    init {
+        getAllSeries()
+    }
+
+    private fun getAllSeries() {
+        disposeObservable(repository.getAllSeries(),::onGetSeriesSuccess,::onError)
+    }
+
+    private fun onGetSeriesSuccess(state :UiState<DataResponse<Series>>){
+        _allSeries.postValue(state)
+    }
+
+    private fun onError(errorMessage: Throwable){
+        _allSeries.postValue(UiState.Error(errorMessage.message.toString()))
+    }
+}
