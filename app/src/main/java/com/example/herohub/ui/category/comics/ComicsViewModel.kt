da5f3b35fb454794501f7f369a47c4dca603c857
Haidy <haidyabugom3a@gmail.com>
2023-05-05 23:49:19 +0300

    add comics view model

---
+++ app/src/main/java/com/example/herohub/ui/category/comics/ComicsViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.herohub.ui.category.comics
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.herohub.data.Repository
+import com.example.herohub.model.Comic
+import com.example.herohub.model.DataResponse
+import com.example.herohub.ui.base.BaseViewModel
+import com.example.herohub.utills.UiState
+
+class ComicsViewModel : BaseViewModel(), ComicInteractionListener {
+    override val TAG: String = this::class.java.simpleName.toString()
+    private val repository = Repository()
+    private val _comics = MutableLiveData<UiState<DataResponse<Comic>>>()
+    val comics: LiveData<UiState<DataResponse<Comic>>>
+        get() = _comics
+
+    private fun getAllComics() {
+        disposeObservable(repository.getAllComics(), ::onGetComicsSuccess, ::onError)
+    }
+
+    private fun onGetComicsSuccess(state: UiState<DataResponse<Comic>>) {
+        _comics.postValue(state)
+    }
+
+    private fun onError(throwable: Throwable) {
+        log("Error : ${throwable.message.toString()}")
+    }
+
+    override fun onClickCharacter(id: Int) {
+        log("$id clicked")
+    }
+}
