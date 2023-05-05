03aa605902021f2fcf30c391cb6c15ec7fc4ff89
hamsa <hamsaalsaadie@gmail.com>
2023-05-05 19:26:39 +0300

    add most popular comics adapter

---
+++ app/src/main/java/com/example/herohub/ui/home/MostPopularComicsAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.herohub.ui.home
+
+import com.example.herohub.R
+import com.example.herohub.model.Comic
+import com.example.herohub.ui.base.BaseAdapter
+import com.example.herohub.ui.base.BaseInteractionListener
+
+class MostPopularComicsAdapter(listener: MostPopularComicsInteractionListener) :
+    BaseAdapter<Comic>(listener) {
+
+    override val layoutId: Int = R.layout.item_most_popular_comics
+}
+
+interface MostPopularComicsInteractionListener : BaseInteractionListener {
+
+    fun onItemClick(id: Int)
+}
