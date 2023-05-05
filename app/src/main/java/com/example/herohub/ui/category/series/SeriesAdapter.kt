6696facf7878c6c6eca5b45614101ce2696fa978
Mohamed Naser <mahmmedn19@gmail.com>
2023-05-06 00:03:21 +0300

    create series category adapter

---
+++ app/src/main/java/com/example/herohub/ui/category/series/SeriesAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.herohub.ui.category.series
+
+import com.example.herohub.R
+import com.example.herohub.model.Series
+import com.example.herohub.ui.base.BaseAdapter
+import com.example.herohub.ui.base.BaseInteractionListener
+
+class SeriesAdapter(listener: SeriesInteractionListener) :
+    BaseAdapter<Series>(listener) {
+    override val layoutId: Int
+        get() = R.layout.item_category
+}
+
+interface SeriesInteractionListener : BaseInteractionListener
