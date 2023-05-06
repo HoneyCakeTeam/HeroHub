1683374c36f877a52da865d0202c9921ceacf223
hamsa <hamsaalsaadie@gmail.com>
2023-05-06 20:00:41 +0300

    implement code

--- app/src/main/java/com/example/herohub/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/herohub/ui/home/HomeFragment.kt
@@ -1,30 +1,71 @@
 package com.example.herohub.ui.home
 
+import android.util.Log
 import androidx.fragment.app.viewModels
 import com.example.herohub.R
 import com.example.herohub.databinding.FragmentHomeBinding
 import com.example.herohub.ui.base.BaseFragment
 import com.example.herohub.ui.home.adapter.HomeAdapter
-import com.example.herohub.ui.home.adapter.SuperHeroesAdapter
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override val TAG: String = this::class.java.simpleName.toString()
     override val layoutIdFragment: Int = R.layout.fragment_home
     override val viewModel: HomeViewModel by viewModels()
-    lateinit var homeAdapter: HomeAdapter
+    private lateinit var homeAdapter: HomeAdapter
+    private val homeItems = mutableListOf<HomeItem>()
 
     override fun setup() {
      initHomeAdapter()
-        viewModel.characterResponse.observe(this){
-            val adapter = SuperHeroesAdapter(it.toData().results)
+        observeLiveData()
+
+    }
+    private fun initHomeAdapter() {
+        homeAdapter = HomeAdapter(mutableListOf(), viewModel)
+        binding.recyclerViewHome.adapter = homeAdapter
+    }
+
+    private fun observeLiveData() {
+        observeSeries()
+        observeCharacterResponse()
+        observeMostPopularComicsResponse()
+    }
+
+    private fun observeCharacterResponse() {
+        viewModel.characterResponse.observe(viewLifecycleOwner) { uiState ->
+            uiState.toData()?.results.let { character ->
+                if (!character.isNullOrEmpty()) {
+                    homeItems.add(HomeItem.SuperHeroes(character))
+                    homeAdapter.setItems(homeItems)
+                }
+                Log.e("TAG", "homeItems characterResponse: $homeItems")
+            }
         }
     }
-    private fun initHomeAdapter(){
-        val list = mutableListOf(HomeItem.Slider(emptyList()), HomeItem.SuperHeroes(emptyList()), HomeItem.MostPopularComics(
-            emptyList()
-        ))
-        homeAdapter = HomeAdapter(list, viewModel)
-        binding.recyclerViewHome.adapter = homeAdapter
+
+    private fun observeMostPopularComicsResponse() {
+        viewModel.mostPopularComicsResponse.observe(viewLifecycleOwner) { uiState ->
+            uiState.toData()?.results.let { comics ->
+                if (!comics.isNullOrEmpty()) {
+                    homeItems.add(HomeItem.MostPopularComics(comics))
+                    homeAdapter.setItems(homeItems)
+                }
+
+                Log.e("TAG", "homeItems mostPopularComicsResponse: $homeItems")
+            }
+        }
+    }
+
+    private fun observeSeries() {
+        viewModel.seriesResponse.observe(viewLifecycleOwner) { uiState ->
+            uiState.toData()?.results.let { series ->
+                if (!series.isNullOrEmpty()) {
+                    homeItems.add(HomeItem.Slider(series))
+                    homeAdapter.setItems(homeItems)
+                }
+            }
+
+            Log.e("TAG", "homeItems seriesResponse: $homeItems")
+        }
     }
 }
