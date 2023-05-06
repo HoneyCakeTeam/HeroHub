984efe0ce9e2a2cfa877bf79b8e462009cef89d6
hamsa <hamsaalsaadie@gmail.com>
2023-05-06 13:31:46 +0300

    implement home adapter

--- app/src/main/java/com/example/herohub/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/herohub/ui/home/HomeFragment.kt
@@ -4,14 +4,27 @@ import androidx.fragment.app.viewModels
 import com.example.herohub.R
 import com.example.herohub.databinding.FragmentHomeBinding
 import com.example.herohub.ui.base.BaseFragment
+import com.example.herohub.ui.home.adapter.HomeAdapter
+import com.example.herohub.ui.home.adapter.SuperHeroesAdapter
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override val TAG: String = this::class.java.simpleName.toString()
     override val layoutIdFragment: Int = R.layout.fragment_home
     override val viewModel: HomeViewModel by viewModels()
+    lateinit var homeAdapter: HomeAdapter
 
     override fun setup() {
-
+     initHomeAdapter()
+        viewModel.characterResponse.observe(this){
+            val adapter = SuperHeroesAdapter(it.toData().results)
+        }
+    }
+    private fun initHomeAdapter(){
+        val list = mutableListOf(HomeItem.Slider(emptyList()), HomeItem.SuperHeroes(emptyList()), HomeItem.MostPopularComics(
+            emptyList()
+        ))
+        homeAdapter = HomeAdapter(list, viewModel)
+        binding.recyclerViewHome.adapter = homeAdapter
     }
 }
