de5104bf9cb4fd274b0980b92efe4a37eeca46af
Mohamed Naser <mahmmedn19@gmail.com>
2023-05-06 14:18:56 +0300

    add argument

--- app/src/main/java/com/example/herohub/ui/seriesdetails/SeriesDetailsFragment.kt
+++ app/src/main/java/com/example/herohub/ui/seriesdetails/SeriesDetailsFragment.kt
@@ -5,19 +5,16 @@ import androidx.navigation.fragment.navArgs
 import com.example.herohub.R
 import com.example.herohub.databinding.FragmentSeriesDetailsBinding
 import com.example.herohub.ui.base.BaseFragment
-import com.example.herohub.ui.characterdetails.CharacterDetailsFragmentArgs
 
 class SeriesDetailsFragment : BaseFragment<FragmentSeriesDetailsBinding>() {
     override val TAG: String = this::class.java.simpleName
     override val layoutIdFragment: Int = R.layout.fragment_series_details
     override val viewModel: SeriesDetailsViewModel by viewModels()
-   // private val args: SeriesDetailsFragmentArgs by navArgs()
+    private val args: SeriesDetailsFragmentArgs by navArgs()
 
-    override fun setup(){
-        viewModel.getSeriesDetails(120)
+    override fun setup() {
+        viewModel.getSeriesDetails(args.seriesId)
     }
-
-
 
 
 }
