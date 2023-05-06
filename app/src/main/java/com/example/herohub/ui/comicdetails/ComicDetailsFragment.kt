e6128b9d7c2b9f6ee41db72adc867e35b92f34a4
Mohamed Ragab <mohamed.ragab.ali.9.@gmail.com>
2023-05-06 17:55:05 +0300

    add save args

--- app/src/main/java/com/example/herohub/ui/comicdetails/ComicDetailsFragment.kt
+++ app/src/main/java/com/example/herohub/ui/comicdetails/ComicDetailsFragment.kt
@@ -1,20 +1,18 @@
 package com.example.herohub.ui.comicdetails
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.herohub.R
 import com.example.herohub.databinding.FragmentComicsDetailsBinding
-import com.example.herohub.model.Comic
 import com.example.herohub.ui.base.BaseFragment
-import com.example.herohub.utills.UiState
 
 class ComicDetailsFragment : BaseFragment<FragmentComicsDetailsBinding>() {
     override val TAG: String = this::class.java.simpleName
     override val layoutIdFragment = R.layout.fragment_comics_details
     override val viewModel: ComicDetailsViewModel by viewModels()
-
+    private val args : ComicDetailsFragmentArgs by navArgs()
     override fun setup() {
-        viewModel.getComic(333)
+        viewModel.getComic(args.comicId)
     }
-
 
 }
