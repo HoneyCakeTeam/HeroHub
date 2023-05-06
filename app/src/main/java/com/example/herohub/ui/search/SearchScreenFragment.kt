fb89b4c04e96cdd743eda8994ddbf1c33aeb205d
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-05-06 12:03:35 +0300

    try to search on Character

--- app/src/main/java/com/example/herohub/ui/search/SearchScreenFragment.kt
+++ app/src/main/java/com/example/herohub/ui/search/SearchScreenFragment.kt
@@ -4,6 +4,7 @@ package com.example.herohub.ui.search
 import androidx.fragment.app.viewModels
 import com.example.herohub.R
 import com.example.herohub.databinding.FragmentSearchScreenBinding
+import com.example.herohub.model.Character
 import com.example.herohub.ui.base.BaseFragment
 
 class SearchScreenFragment  : BaseFragment<FragmentSearchScreenBinding>() {
@@ -12,12 +13,23 @@ class SearchScreenFragment  : BaseFragment<FragmentSearchScreenBinding>() {
     override val viewModel: SearchViewModel by viewModels()
 
     override fun setup() {
-        viewModel.getAllCharacters()
         initAdapter()
     }
 
-    private fun initAdapter(){
+    private fun initAdapter() {
         val adapter = SearchAdapter(viewModel)
         binding.rvSearchResult.adapter = adapter
+        viewModel.getSearchResult()
+        }
     }
-}
+
+
+//        binding.searchView.addTextChangedListener(object : TextWatcher {
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                viewModel.setSearchQuery(s.toString())
+//            }
+//
+//            override fun afterTextChanged(s: Editable?) {}
+//        })
--- app/src/main/java/com/example/herohub/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/search/SearchViewModel.kt
@@ -1,40 +1,97 @@
 package com.example.herohub.ui.search
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.herohub.data.Repository
 import com.example.herohub.model.Character
 import com.example.herohub.model.DataResponse
 import com.example.herohub.ui.base.BaseViewModel
 import com.example.herohub.utills.UiState
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class SearchViewModel : BaseViewModel(), SearchAdapter.SearchInteractionListener {
     override val TAG: String = this::class.java.simpleName.toString()
     private val repository = Repository()
     private val _response = MutableLiveData<UiState<DataResponse<Character>?>>()
-    private val response: LiveData<UiState<DataResponse<Character>?>> get() = _response
+    val response: LiveData<UiState<DataResponse<Character>?>>
+        get() = _response
+    private val _searchQuery = MutableLiveData<String>()
+    val searchQuery : LiveData<String>
+        get() = _searchQuery
 
-    fun getAllCharacters() {
+    private val _searchResult = MediatorLiveData<List<Character>>()
+
+    init {
+        getAllCharacters()
+        search()
+    }
+
+    private fun search(){
+        _searchResult.addSource(_searchQuery) {query ->
+            if (query.isEmpty()) {
+                _searchResult.value = _response.value?.toData()?.results!!
+            }else {
+                repository.searchQuery(query)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                        { response ->
+                            _searchResult.value = response.toData()?.results!!
+                        },
+                        { error ->
+                            log(error.message!!)
+                        }
+                    ).dispose()
+            }
+        }
+    }
+
+    fun setSearchQuery(query: String) {
+        _searchQuery.value = query
+    }
+    fun getSearchResult(): LiveData<List<Character>> = _searchResult
+
+    private fun getAllCharacters() {
         _response.postValue(UiState.Loading)
-        disposeObservable(repository.getAllCharacters(),
-            ::onGetCharacterSuccess , ::onGetCharacterFailure)
-        log(response.toString())
+        disposeObservable(
+            repository.getAllCharacters(),
+            ::onGetCharacterSuccess, ::onGetCharacterFailure
+        )
     }
 
-    private fun onGetCharacterSuccess(uiState: UiState<DataResponse<Character>>) {
-        _response.postValue(uiState)
-    }
+        private fun onGetCharacterSuccess(uiState: UiState<DataResponse<Character>>) {
+            _response.postValue(uiState)
+            log(uiState.toData().toString())
+        }
 
-    private fun onGetCharacterFailure(throwable: Throwable) {
-        _response.postValue(UiState.Error(throwable.message.toString()))
-    }
+        private fun onGetCharacterFailure(throwable: Throwable) {
+            _response.postValue(UiState.Error(throwable.message.toString()))
+
+        }
 
     override fun <T> onClickItem(item: T) {
         TODO("Not yet implemented")
     }
-
 }
 
+//    val searchResult: LiveData<List<Character>>
+//        get() = _searchResult
+
+//        repository.searchQuery(query)
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe(
+//                { response ->
+//                    _searchResult.value = response.toData()?.results!!
+//                },
+//                { error ->
+//                    Log.e("SearchViewModel", "Search failed", error)
+//                }
+//            ).dispose()
+
+//    private val _searchResult = MutableLiveData<List<Character>>()
 
 //    private val _searchResults = MutableLiveData<List<Character>>()
 //    val searchResults: LiveData<List<Character>> get() = _searchResults
