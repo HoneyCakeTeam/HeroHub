1d6025688d5d0dee66610835f54dfce045f211e9
hamsa <hamsaalsaadie@gmail.com>
2023-05-06 13:30:21 +0300

    add liveData and getAllSeries function

--- app/src/main/java/com/example/herohub/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/home/HomeViewModel.kt
@@ -4,37 +4,82 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.herohub.data.Repository
 import com.example.herohub.model.Character
+import com.example.herohub.model.Comic
 import com.example.herohub.model.DataResponse
+import com.example.herohub.model.Series
+import com.example.herohub.ui.base.BaseInteractionListener
 import com.example.herohub.ui.base.BaseViewModel
 import com.example.herohub.utills.UiState
 
-class HomeViewModel : BaseViewModel() {
+class HomeViewModel : BaseViewModel(), BaseInteractionListener{
     override val TAG: String
         get() = this::class.java.simpleName.toString()
 
     private val repository: Repository by lazy { Repository() }
 
-    private val _characterResponse = MutableLiveData<UiState<DataResponse<Character>?>>()
-    val characterResponse: LiveData<UiState<DataResponse<Character>?>>
+    private val _characterResponse = MutableLiveData<UiState<DataResponse<Character>>>()
+    val characterResponse: LiveData<UiState<DataResponse<Character>>>
         get() = _characterResponse
 
+    private val _seriesResponse = MutableLiveData<UiState<DataResponse<Series>>>()
+    val seriesResponse: LiveData<UiState<DataResponse<Series>>>
+        get() = _seriesResponse
+
+    private val _mostPopularComicsResponse = MutableLiveData<UiState<DataResponse<Comic>>>()
+    val mostPopularComicsResponse: LiveData<UiState<DataResponse<Comic>>>
+        get() = _mostPopularComicsResponse
+
     init {
+        getHomeData()
+    }
+
+    private fun getHomeData() {
         getAllCharacters()
+        getMostPopularComics()
+        getAllSeries()
     }
 
     private fun getAllCharacters() {
         disposeObservable(
             repository.getAllCharacters(),
             ::onGetCharacterSuccess,
-            ::onGetCharacterError
+            ::onError
         )
     }
 
-    private fun onGetCharacterSuccess(UiState: UiState<DataResponse<Character>?>) {
+    private fun getMostPopularComics() {
+        disposeObservable(
+            repository.getAllComics(),
+            ::onGetMostPopularComicsSuccess,
+            ::onError
+        )
+    }
+
+    private fun getAllSeries() {
+        disposeObservable(
+            repository.getAllSeries(),
+            ::onGetSeriesSuccess,
+            ::onError
+        )
+    }
+
+
+    private fun onGetCharacterSuccess(UiState: UiState<DataResponse<Character>>) {
         _characterResponse.postValue(UiState)
     }
 
-    private fun onGetCharacterError(errorMessage: Throwable) {
-        _characterResponse.postValue(UiState.Error(errorMessage.message.toString()))
+    private fun onGetMostPopularComicsSuccess(UiState: UiState<DataResponse<Comic>>) {
+        _mostPopularComicsResponse.postValue(UiState)
     }
+
+    private fun onGetSeriesSuccess(UiState: UiState<DataResponse<Series>>) {
+        _seriesResponse.postValue(UiState)
+    }
+
+    private fun onError(throwable: Throwable) {
+        _characterResponse.postValue(UiState.Error(throwable.message.toString()))
+        _mostPopularComicsResponse.postValue(UiState.Error(throwable.message.toString()))
+        _seriesResponse.postValue(UiState.Error(throwable.message.toString()))
+    }
+
 }
