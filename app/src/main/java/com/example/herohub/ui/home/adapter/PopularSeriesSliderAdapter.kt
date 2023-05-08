7c618908e381e72baa931089328f80bcae6af154
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-08 20:18:22 +0300

    implement PopularSeriesSliderAdapter

--- app/src/main/java/com/example/herohub/ui/home/adapter/PopularSeriesSliderAdapter.kt
+++ app/src/main/java/com/example/herohub/ui/home/adapter/PopularSeriesSliderAdapter.kt
@@ -1,4 +1,16 @@
 package com.example.herohub.ui.home.adapter
 
-class PopularSeriesSliderAdapter {
+import com.example.herohub.R
+import com.example.herohub.model.Series
+import com.example.herohub.ui.base.BaseAdapter
+import com.example.herohub.ui.base.BaseInteractionListener
+
+class PopularSeriesSliderAdapter(listener: PopularSeriesInteractionListener) :
+    BaseAdapter<Series>(listener) {
+
+    override val layoutId: Int = R.layout.item_popular_series_slider
+}
+
+interface PopularSeriesInteractionListener : BaseInteractionListener {
+    fun onPopularSeriesSliderItemClick(id: Int)
 }
