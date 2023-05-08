d325b8da2e0963d85105d597332dcf37f48537d8
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-09 00:53:13 +0300

    remove unused imports

--- app/src/main/java/com/example/herohub/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/home/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.herohub.ui.home
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.herohub.data.Repository
