acc80a3f15702fa83dc36a94c191f5bd02b711ff
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-09 00:31:05 +0300

    add logic of live data into view model

--- app/src/main/java/com/example/herohub/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/herohub/ui/home/HomeFragment.kt
@@ -16,87 +16,14 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun setup() {
         initHomeAdapter()
-        observeLiveData()
     }
 
     private fun initHomeAdapter() {
         homeAdapter = HomeAdapter(homeItems, viewModel)
         binding.recyclerViewHome.adapter = homeAdapter
-    }
 
-    private fun observeLiveData() {
-        observeCharactersByAppearance()
-        observeSliderSeries()
-        observeSuperHeroesResponse()
-        observeMostPopularCharactersResponse()
-    }
-
-    private fun observeSliderSeries() {
-        viewModel.seriesResponse.observe(viewLifecycleOwner) { uiState ->
-            uiState.toData()?.results?.let { series ->
-                if (series.isNotEmpty()) {
-                    val images = series.filterNot {
-                        it.thumbnail?.path?.contains("image_not_available")!!
-                    }.shuffled().take(10)
-                    homeItems.add(HomeItem.PopularSeries(images))
-                    homeAdapter.setItems(homeItems)
-                }
-            }
+        viewModel.homeItemsLiveData.observe(viewLifecycleOwner) { homeItem ->
+            homeAdapter.setItems(homeItem)
         }
-    }
-
-    private fun observeCharactersByAppearance() {
-        viewModel.characterResponse.observe(viewLifecycleOwner) { uiState ->
-            uiState.toData()?.results.let { character ->
-                if (!character.isNullOrEmpty()) {
-                    homeItems.add(HomeItem.CharactersByAppearance(character.filterNot {
-                        it.thumbnail?.path?.contains("image_not_available")!!
-                    }.filter {
-                        it.run {
-                            comics?.available!! > 20
-                        }
-                    }.takeLast(20)))
-                    homeAdapter.setItems(homeItems)
-                }
-            }
-        }
-    }
-
-    private fun observeSuperHeroesResponse() {
-        viewModel.characterResponse.observe(viewLifecycleOwner) { uiState ->
-            uiState.toData()?.results.let { character ->
-                if (!character.isNullOrEmpty()) {
-                    homeItems.add(HomeItem.SuperHeroes(character.filterNot {
-                        it.thumbnail?.path?.contains("image_not_available")!!
-                    }.take(15)))
-                    homeAdapter.setItems(homeItems)
-                }
-            }
-        }
-    }
-
-    private fun observeMostPopularCharactersResponse() {
-        viewModel.characterResponse.observe(viewLifecycleOwner) { uiState ->
-            uiState.toData()?.results.let { character ->
-                if (!character.isNullOrEmpty()) {
-                    homeItems.add(HomeItem.MostPopularCharacters(character.filterNot {
-                        it.thumbnail?.path?.contains("image_not_available")!!
-                    }.filter {
-                        it.run {
-                            (comics?.available!! +
-                                    series?.available!! +
-                                    events?.available!! +
-                                    stories?.available!!) > 150
-                        }
-                    }.take(20)))
-                    homeAdapter.setItems(homeItems)
-                }
-            }
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        homeItems.clear()
     }
 }
--- app/src/main/java/com/example/herohub/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/home/HomeViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.herohub.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.herohub.data.Repository
 import com.example.herohub.model.Character
+import com.example.herohub.model.Comic
 import com.example.herohub.model.DataResponse
+import com.example.herohub.model.Event
 import com.example.herohub.model.Series
 import com.example.herohub.ui.base.BaseViewModel
 import com.example.herohub.ui.home.adapter.CharactersByAppearanceInteractionListener
@@ -20,6 +23,7 @@ class HomeViewModel : BaseViewModel(), MostPopularCharactersInteractionListener,
         get() = this::class.java.simpleName.toString()
 
     private val repository: Repository by lazy { Repository() }
+    private val homeItems = mutableListOf<HomeItem>()
 
     private val _characterResponse = MutableLiveData<UiState<DataResponse<Character>>>()
     val characterResponse: LiveData<UiState<DataResponse<Character>>>
@@ -29,16 +33,30 @@ class HomeViewModel : BaseViewModel(), MostPopularCharactersInteractionListener,
     val seriesResponse: LiveData<UiState<DataResponse<Series>>>
         get() = _seriesResponse
 
+    private val _eventResponse = MutableLiveData<UiState<DataResponse<Event>>>()
+    val eventResponse: LiveData<UiState<DataResponse<Event>>>
+        get() = _eventResponse
+
+    private val _comicsResponse = MutableLiveData<UiState<DataResponse<Comic>>>()
+    val comicsResponse: LiveData<UiState<DataResponse<Comic>>>
+        get() = _comicsResponse
+
+    private val _homeItemsLiveData = MutableLiveData<List<HomeItem>>()
+    val homeItemsLiveData: LiveData<List<HomeItem>>
+        get() = _homeItemsLiveData
+
     init {
         getHomeData()
     }
 
     private fun getHomeData() {
+        Log.e(TAG, "Home items  in getHomeData :${homeItems}")
         getAllCharacters()
         getAllSeries()
     }
 
     private fun getAllCharacters() {
+        _characterResponse.postValue(UiState.Loading)
         disposeObservable(
             repository.getAllCharacters(),
             ::onGetCharacterSuccess,
@@ -55,16 +73,49 @@ class HomeViewModel : BaseViewModel(), MostPopularCharactersInteractionListener,
     }
 
     private fun onGetCharacterSuccess(UiState: UiState<DataResponse<Character>>) {
-        _characterResponse.postValue(UiState)
+        _characterResponse.value = UiState
+        val character = _characterResponse.value?.toData()?.results
+        homeItems.add(HomeItem.CharactersByAppearance(character?.filterNot {
+            it.thumbnail?.path?.contains("image_not_available")!!
+        }!!.filter {
+            it.run {
+                comics?.available!! > 20
+            }
+        }.takeLast(20)))
+
+        homeItems.add(HomeItem.SuperHeroes(character.filterNot {
+            it.thumbnail?.path?.contains("image_not_available")!!
+        }.take(15)))
+
+        homeItems.add(HomeItem.MostPopularCharacters(character.filterNot {
+            it.thumbnail?.path?.contains("image_not_available")!!
+        }.filter {
+            it.run {
+                (comics?.available!! +
+                        series?.available!! +
+                        events?.available!! +
+                        stories?.available!!) > 150
+            }
+        }.take(20)))
+        _homeItemsLiveData.postValue(homeItems)
     }
 
     private fun onGetSeriesSuccess(UiState: UiState<DataResponse<Series>>) {
-        _seriesResponse.postValue(UiState)
+        _seriesResponse.value = UiState
+        val images = _seriesResponse.value?.toData()?.results?.filterNot {
+            it.thumbnail?.path?.contains("image_not_available")!!
+        }?.shuffled()?.take(10)
+        homeItems.add(HomeItem.PopularSeries(images ?: emptyList()))
+        _homeItemsLiveData.postValue(homeItems)
+
+        log("Home items : ${homeItems}")
     }
 
     private fun onError(throwable: Throwable) {
         _characterResponse.postValue(UiState.Error(throwable.message.toString()))
         _seriesResponse.postValue(UiState.Error(throwable.message.toString()))
+        _eventResponse.postValue(UiState.Error(throwable.message.toString()))
+        _comicsResponse.postValue(UiState.Error(throwable.message.toString()))
     }
 
     override fun onMostPopularCharactersItemClick(id: Int) {
