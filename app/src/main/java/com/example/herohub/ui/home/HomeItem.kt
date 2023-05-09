a5e4f518b33a0a4db37cb8541d810def07fd50b9
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-05-09 19:11:40 +0300

    add mostPopularEvent data class

--- app/src/main/java/com/example/herohub/ui/home/HomeItem.kt
+++ app/src/main/java/com/example/herohub/ui/home/HomeItem.kt
@@ -1,6 +1,7 @@
 package com.example.herohub.ui.home
 
 import com.example.herohub.model.Character
+import com.example.herohub.model.Event
 import com.example.herohub.model.Series
 
 sealed class HomeItem(val position: Int) {
@@ -8,4 +9,5 @@ sealed class HomeItem(val position: Int) {
     data class SuperHeroes(val superHeroes: List<Character>) : HomeItem(1)
     data class MostPopularCharacters(val mostPopularCharacters: List<Character>) : HomeItem(2)
     data class CharactersByAppearance(val charactersByAppearance: List<Character>) : HomeItem(3)
+    data class MostPopularEvents(val mostPopularEvents: List<Event>) : HomeItem(4)
 }
