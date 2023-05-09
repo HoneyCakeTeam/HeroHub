953e6b1753018f217778d9862c9243abb676a408
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-09 15:36:11 +0300

    delete unused CategoryInteractionListener

--- app/src/main/java/com/example/herohub/ui/CategoryInteractionListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.herohub.ui
-
-import com.example.herohub.ui.base.BaseInteractionListener
-
-interface CategoryInteractionListener: BaseInteractionListener {
-    fun onClickItem(id: Int)
-}
