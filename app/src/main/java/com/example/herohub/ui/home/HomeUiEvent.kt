6c4ce6d0442c1fe54ed0f7c1a3d7db1ed854f209
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-05-10 20:48:53 +0300

    add in sealed interface

--- app/src/main/java/com/example/herohub/ui/home/HomeUiEvent.kt
+++ app/src/main/java/com/example/herohub/ui/home/HomeUiEvent.kt
@@ -2,4 +2,7 @@ package com.example.herohub.ui.home
 
 sealed interface HomeUiEvent {
     object ClickSeeAllCharacterEvent : HomeUiEvent
+    object ClickSeeAllEventsEvent : HomeUiEvent
+
+    data class ClickEventEvent(val id: Int) : HomeUiEvent
 }
