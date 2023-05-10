7aca00b5eb58eab911089bb50baf41d377c8f8ca
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-05-10 21:10:28 +0300

    add click on see all

--- app/src/main/java/com/example/herohub/ui/home/HomeUiEvent.kt
+++ app/src/main/java/com/example/herohub/ui/home/HomeUiEvent.kt
@@ -3,6 +3,8 @@ package com.example.herohub.ui.home
 sealed interface HomeUiEvent {
     object ClickSeeAllCharacterEvent : HomeUiEvent
     object ClickSeeAllEventsEvent : HomeUiEvent
+    object ClickSeeAllComicsEvent : HomeUiEvent
 
     data class ClickEventEvent(val id: Int) : HomeUiEvent
+    data class ClickComicEvent(val id: Int) : HomeUiEvent
 }
