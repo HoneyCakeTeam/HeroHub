e50232c57734705c86cdaacbf63a572ef04a223e
Asia sama <sehunexo710@gmail.com>
2023-05-11 17:23:25 +0300

    add args in character details screen and handel event

--- app/src/main/java/com/example/herohub/ui/characterdetails/CharacterDetailsViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/characterdetails/CharacterDetailsViewModel.kt
@@ -2,19 +2,24 @@ package com.example.herohub.ui.characterdetails
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import com.example.herohub.data.Repository
 import com.example.herohub.model.Character
 import com.example.herohub.model.Comic
 import com.example.herohub.model.DataResponse
 import com.example.herohub.model.FavoriteItem
 import com.example.herohub.ui.base.BaseViewModel
+import com.example.herohub.ui.home.HomeItem
+import com.example.herohub.utills.EventHandler
 import com.example.herohub.utills.UiState
 
 
 /**
  * Created by Aziza Helmy on 5/3/2023.
  */
-class CharacterDetailsViewModel : BaseViewModel(), ComicsInteractionListener {
+class CharacterDetailsViewModel(state: SavedStateHandle) : BaseViewModel(),
+    ComicsInteractionListener {
+
     private val repository = Repository()
 
     private val _characterComics = MutableLiveData<UiState<DataResponse<Comic>>>()
@@ -29,27 +34,49 @@ class CharacterDetailsViewModel : BaseViewModel(), ComicsInteractionListener {
     val navigateToComicDetails: LiveData<Int> = _navigateToComicDetails
 
     private val characterItem = MutableLiveData<Character>()
-    val isFavorite = MutableLiveData<Boolean>()
+
+    private val _isFavorite = MutableLiveData<Boolean>()
+    val isFavorite: LiveData<Boolean> = _isFavorite
+
+    private val _homeItemsLiveData = MutableLiveData<List<HomeItem>>()
+    val homeItemsLiveData: LiveData<List<HomeItem>>
+        get() = _homeItemsLiveData
+
+    private val _characterDetailsUiEvent =
+        MutableLiveData<EventHandler<CharacterDetailsUiEvent?>>(EventHandler(null))
+    val characterDetailsUiEvent: LiveData<EventHandler<CharacterDetailsUiEvent?>>
+        get() = _characterDetailsUiEvent
+
+    private val characterArgs = CharacterDetailsFragmentArgs.fromSavedStateHandle(state)
+
+    init {
+        getDetailsOfCharacter()
+        getComicsOfCharacter()
+    }
 
     override val TAG: String
         get() = this::class.simpleName.toString()
 
-    fun getComicsOfCharacter(characterId: Int) {
+    private fun getComicsOfCharacter() {
         disposeSingle(
-            repository.getCharacterComics(characterId), ::onGetCharacterComicsSuccess, ::onError
+            repository.getCharacterComics(characterArgs.characterId),
+            ::onGetCharacterComicsSuccess,
+            ::onError
         )
     }
 
-    fun getDetailsOfCharacter(characterId: Int) {
+    private fun getDetailsOfCharacter() {
         disposeSingle(
-            repository.getCharacterDetails(characterId), ::onGetCharacterDetailsSuccess, ::onError
+            repository.getCharacterDetails(characterArgs.characterId),
+            ::onGetCharacterDetailsSuccess,
+            ::onError
         )
     }
 
     private fun onGetCharacterDetailsSuccess(state: UiState<DataResponse<Character>>) {
         _characterDetails.value = state
         characterItem.value = characterDetails.value?.toData()?.results?.firstOrNull()
-        isFavorite.value = repository.isFavorite(characterItem.value?.id.toString())
+        _isFavorite.value = repository.isFavorite(characterItem.value?.id.toString())
     }
 
     private fun onGetCharacterComicsSuccess(state: UiState<DataResponse<Comic>>) {
@@ -62,7 +89,13 @@ class CharacterDetailsViewModel : BaseViewModel(), ComicsInteractionListener {
     }
 
     override fun onClickComic(id: Int) {
-        _navigateToComicDetails.postValue(id)
+        _characterDetailsUiEvent.postValue(
+            EventHandler(
+                CharacterDetailsUiEvent.ClickCharacterComic(
+                    id
+                )
+            )
+        )
     }
 
 
@@ -75,10 +108,10 @@ class CharacterDetailsViewModel : BaseViewModel(), ComicsInteractionListener {
 
         if (isFavorite.value == true) {
             repository.removeFavorite(favoriteItem)
-            isFavorite.postValue(false)
+            _isFavorite.postValue(false)
         } else {
             repository.addToFavorite(favoriteItem)
-            isFavorite.postValue(true)
+            _isFavorite.postValue(true)
         }
     }
 
