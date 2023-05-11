96fc743b9e6351144d093af75899ab175d98c177
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-11 19:23:00 +0300

    override nested recycler in character details

--- app/src/main/java/com/example/herohub/ui/characterdetails/adapter/CharacterDetailsAdapter.kt
+++ app/src/main/java/com/example/herohub/ui/characterdetails/adapter/CharacterDetailsAdapter.kt
@@ -1,10 +1,24 @@
 package com.example.herohub.ui.characterdetails.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.herohub.BR
 import com.example.herohub.R
 import com.example.herohub.ui.base.BaseAdapter
 import com.example.herohub.ui.base.BaseInteractionListener
 import com.example.herohub.ui.characterdetails.CharacterDetailsItem
+import com.example.herohub.ui.home.HomeItem
+import com.example.herohub.ui.home.adapter.MostPopularComicsAdapter
+import com.example.herohub.ui.home.adapter.MostPopularComicsInteractionListener
+import com.example.herohub.ui.home.adapter.MostPopularEventsAdapter
+import com.example.herohub.ui.home.adapter.MostPopularEventsInteractionListener
+import com.example.herohub.ui.home.adapter.MostPopularSeriesAdapter
+import com.example.herohub.ui.home.adapter.MostPopularSeriesInteractionListener
+import com.example.herohub.ui.home.adapter.SliderAdapter
+import com.example.herohub.ui.home.adapter.SliderInteractionListener
+import com.example.herohub.ui.home.adapter.SuperHeroesAdapter
+import com.example.herohub.ui.home.adapter.SuperHeroesInteractionListener
 
 /**
  * Created by Aziza Helmy on 5/11/2023.
@@ -12,8 +26,7 @@ import com.example.herohub.ui.characterdetails.CharacterDetailsItem
 class CharacterDetailsAdapter(
     private var characterDetailsItems: MutableList<CharacterDetailsItem>,
     listener: BaseInteractionListener
-) :
-    BaseAdapter<CharacterDetailsItem>(listener) {
+) : BaseAdapter<CharacterDetailsItem>(listener) {
 
     override val layoutId: Int = 0
 
@@ -23,12 +36,26 @@ class CharacterDetailsAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
-
-        return super.onCreateViewHolder(parent, viewType)
+        return ItemViewHolder(
+            DataBindingUtil.inflate(LayoutInflater.from(parent.context), viewType, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
-        super.onBindViewHolder(holder, position)
+        if (characterDetailsItems.isNotEmpty())
+            bind(holder as ItemViewHolder, position)
+    }
+    private fun bind(holder: ItemViewHolder, position: Int) {
+        if (position != -1) {
+            when (val currentItem = characterDetailsItems[position]) {
+                is CharacterDetailsItem.CharacterComics -> {
+
+                }
+                is CharacterDetailsItem.CharacterEvents -> TODO()
+                is CharacterDetailsItem.CharacterInfo -> TODO()
+                is CharacterDetailsItem.CharacterSeries -> TODO()
+            }
+        }
     }
 
     override fun getItemViewType(position: Int): Int {
