bdaa70fa7fc5e63be874d3e775d006d6885fd47d
Nour Elden Alaa <nourelden515@gmail.com>
2023-05-11 16:26:26 +0300

    handle state in EventDetailsFragment.kt

--- app/src/main/java/com/example/herohub/ui/eventdetails/EventDetailsViewModel.kt
+++ app/src/main/java/com/example/herohub/ui/eventdetails/EventDetailsViewModel.kt
@@ -2,15 +2,18 @@ package com.example.herohub.ui.eventdetails
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import com.example.herohub.data.Repository
 import com.example.herohub.model.DataResponse
 import com.example.herohub.model.Event
 import com.example.herohub.ui.base.BaseViewModel
 import com.example.herohub.utills.UiState
 
-class EventDetailsViewModel : BaseViewModel() {
+class EventDetailsViewModel(state: SavedStateHandle) : BaseViewModel() {
     private val repository = Repository()
     override val TAG = "EVENT_DETAILS_VIEW_MODEL"
+
+    private val eventArgs = EventDetailsFragmentArgs.fromSavedStateHandle(state)
 
     private val _eventResponse = MutableLiveData<UiState<DataResponse<Event>>>()
     val eventResponse: LiveData<UiState<DataResponse<Event>>>
@@ -20,19 +23,23 @@ class EventDetailsViewModel : BaseViewModel() {
     val event: LiveData<Event>
         get() = _event
 
-    fun getEvent(eventId: Int) {
+    init {
+        getEvent()
+    }
+
+    private fun getEvent() {
         _eventResponse.postValue(UiState.Loading)
         disposeSingle(
-            repository.getEvent(eventId),
+            repository.getEvent(eventArgs.eventId),
             ::onGetEventSuccess,
             ::onGetEventFailure
         )
 
     }
 
-    private fun onGetEventSuccess(UiState: UiState<DataResponse<Event>>) {
-        _eventResponse.value = UiState
-        UiState.toData()?.let {
+    private fun onGetEventSuccess(uiState: UiState<DataResponse<Event>>) {
+        _eventResponse.value = uiState
+        uiState.toData()?.let {
             _event.value = it.results?.get(0)
         }
     }
