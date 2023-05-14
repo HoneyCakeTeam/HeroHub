71da93cdbab995188590468630ad0c8b7e2d2fcc
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-14 20:13:52 +0300

    change file path

--- app/src/main/java/com/example/herohub/data/source/remote/MarvelApi.kt
+++ app/src/main/java/com/example/herohub/data/source/remote/MarvelApi.kt
@@ -1,6 +1,5 @@
 package com.example.herohub.data.source.remote
 
-import android.app.Application
 import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -13,12 +12,14 @@ object MarvelApi {
     private const val BASE_URL = "https://gateway.marvel.com/v1/public/"
     private const val HTTP_CACHE = "http-cache"
     private const val MAX_SIZE = 10L * 1024L * 1024L// 10 MiB
+    private val CACHE_DIRECTORY = File("/data/data/com.example.herohub/cache", HTTP_CACHE)
+    private val CACHE = Cache(CACHE_DIRECTORY, MAX_SIZE)
 
     private val logInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BASIC
     }
     private val client = OkHttpClient().newBuilder().apply {
-        cache(Cache(File(Application().applicationContext.cacheDir, HTTP_CACHE), MAX_SIZE))
+        cache(CACHE)
         addInterceptor(logInterceptor)
         addInterceptor(MarvelInterceptor())
         addNetworkInterceptor(CacheInterceptor())
