fc5cffcb9f51d527a0887e91263fb02f064bd80d
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-17 12:29:05 +0300

    implement CharacterDao with all necessary functions

--- app/src/main/java/com/example/herohub/data/local/dao/CharacterDao.kt
+++ app/src/main/java/com/example/herohub/data/local/dao/CharacterDao.kt
@@ -1,7 +1,30 @@
 package com.example.herohub.data.local.dao
 
-import androidx.room.Dao
+import androidx.room.*
+import com.example.herohub.data.local.CharacterEntity
+import io.reactivex.rxjava3.core.*
 
 @Dao
 interface CharacterDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertCharacter(character: CharacterEntity): Completable
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllCharacters(characters: List<CharacterEntity>): Completable
+
+    @Delete
+    fun deleteCharacter(character: CharacterEntity): Completable
+
+    @Query("DELETE FROM CHARACTER_TABLE")
+    fun deleteAllCharacters(): Completable
+
+    @Update
+    fun updateCharacter(character: CharacterEntity): Completable
+
+    @Query("SELECT * FROM CHARACTER_TABLE")
+    fun getAllCharacters(): Observable<List<CharacterEntity>>
+
+    @Query("SELECT * FROM CHARACTER_TABLE WHERE id = :characterId")
+    fun getCharacterById(characterId: Int): Observable<CharacterEntity>
 }
