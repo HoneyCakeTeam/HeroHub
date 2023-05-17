232f8abb13811be55303c818210448cb7a6fa216
Asia sama <sehunexo710@gmail.com>
2023-05-17 21:28:28 +0300

    create mapper for FavoriteItem

--- app/src/main/java/com/example/herohub/data/remote/model/FavoriteItem.kt
+++ app/src/main/java/com/example/herohub/data/remote/model/FavoriteItem.kt
@@ -1,7 +1,24 @@
 package com.example.herohub.data.remote.model
 
-data class FavoriteItem(var id: String, var title: String, var imageUrl: String, var type: Type) {
+import com.example.herohub.data.local.FavoriteEntity
+
+data class FavoriteItem(
+    var id: String,
+    var title: String,
+    var imageUrl: String,
+    var type: Type,
+) {
     enum class Type {
         CHARACTER, EVENT, COMIC, SERIES
     }
 }
+
+fun FavoriteEntity.asFavoriteItem(): FavoriteItem {
+    return FavoriteItem(
+        id = id.toString(),
+        title = title,
+        imageUrl = imageUrl,
+        type = FavoriteItem.Type.valueOf(type)
+    )
+}
+
