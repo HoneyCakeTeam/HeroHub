318217278687ae80c23783b6302b2e942822365f
Hagar-Gomaa <hagargomaa313@gmail.com>
2023-05-17 11:23:39 +0300

    series entity

---
+++ app/src/main/java/com/example/herohub/data/local/SeriesEntity.kt
@@ -0,0 +1,17 @@
+package com.example.herohub.data.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.herohub.data.remote.model.Comic
+import com.example.herohub.data.remote.model.Thumbnail
+
+@Entity(tableName = "SERIES_TABLE")
+data class SeriesEntity(
+    @PrimaryKey val id: Int,
+    val title: String,
+    val description: String,
+    val modified: String,
+    @ColumnInfo("imagePath") val thumbnail: Thumbnail
+
+    )
