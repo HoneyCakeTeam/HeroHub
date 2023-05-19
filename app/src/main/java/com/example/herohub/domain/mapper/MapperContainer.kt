dc7fd691508f9ef8f23f4b721de2b05634792f46
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-19 04:27:24 +0300

    create and implement MapperContainer

---
+++ app/src/main/java/com/example/herohub/domain/mapper/MapperContainer.kt
@@ -0,0 +1,10 @@
+package com.example.herohub.domain.mapper
+
+import javax.inject.Inject
+
+data class MapperContainer @Inject constructor(
+    val characterMapper: CharacterMapper,
+    val comicMapper: ComicMapper,
+    val eventMapper: EventMapper,
+    val seriesMapper: SeriesMapper,
+)
