da305f4b919dbc5ac366623eda3c9fbccda8f83f
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-20 00:13:50 +0300

    update dto to domain

--- app/src/main/java/com/example/herohub/domain/dto_to_domain_mapper/EventDtoToEvent.kt
+++ app/src/main/java/com/example/herohub/domain/dto_to_domain_mapper/EventDtoToEvent.kt
@@ -1,20 +1,19 @@
 package com.example.herohub.domain.dto_to_domain_mapper
 
-import com.example.herohub.data.remote.model.CharacterDto
+import com.example.herohub.data.remote.model.EventDto
 import com.example.herohub.domain.Mapper
-import com.example.herohub.domain.model.Character
+import com.example.herohub.domain.model.Event
 import javax.inject.Inject
 
 /**
- * Created by Asia sama on 5/19/2023.
- * sehunexo710@gmail.com
+ * Created by Aziza Helmy on 5/19/2023.
  */
-class CharacterMapper @Inject constructor() : Mapper<List<CharacterDto>, List<Character>> {
-    override fun map(input: List<CharacterDto>): List<Character> {
+class EventDtoToEvent @Inject constructor() : Mapper<List<EventDto>, List<Event>> {
+    override fun map(input: List<EventDto>): List<Event> {
         return input.map {
-            Character(
+            Event(
                 id = it.id ?: 0,
-                name = it.name ?: "",
+                title = it.title ?: "",
                 description = it.description ?: "",
                 modified = it.modified ?: "",
                 imageUrl = "${it.thumbnail?.path}.${it.thumbnail?.extension}",
--- app/src/main/java/com/example/herohub/domain/dto_to_domain_mapper/MapperUiContainer.kt
+++ app/src/main/java/com/example/herohub/domain/dto_to_domain_mapper/MapperUiContainer.kt
@@ -1,14 +1,10 @@
 package com.example.herohub.domain.dto_to_domain_mapper
 
-import com.example.herohub.data.local.dto_to_entity_mapper.CharacterMapper
-import com.example.herohub.data.local.dto_to_entity_mapper.ComicMapper
-import com.example.herohub.data.local.dto_to_entity_mapper.EventMapper
-import com.example.herohub.data.local.dto_to_entity_mapper.SeriesMapper
 import javax.inject.Inject
 
 data class MapperUiContainer @Inject constructor(
-    val characterMapper: CharacterMapper,
-    val comicMapper: ComicMapper,
-    val eventMapper: EventMapper,
-    val seriesMapper: SeriesMapper,
+    val characterDtoToCharacter: CharacterDtoToCharacter,
+    val comicDtoToComic: ComicDtoToComic,
+    val eventDtoToEvent: EventDtoToEvent,
+    val seriesDtoToSeries: SeriesDtoToSeries,
 )
