47de593d9296e9dee69585aa794a29711780e854
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-05-19 04:25:26 +0300

    remove chucker library

--- app/src/main/java/com/example/herohub/HeroHub.kt
+++ app/src/main/java/com/example/herohub/HeroHub.kt
@@ -1,19 +1,7 @@
 package com.example.herohub
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-
-class HeroHub : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        if (instance == null) {
-            instance = this
-        }
-    }
-
-    companion object {
-        var instance: HeroHub? = null
-            private set
-    }
-}
+@HiltAndroidApp
+class HeroHub : Application() {}
