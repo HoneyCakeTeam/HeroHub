5b1c84bdd1154c787a28ccd2fb1f70b07bfa1972
Aziza <aziza.helmy35@gmail.com>
2023-05-20 00:07:54 +0300

    remove unnecessary code

--- app/src/main/java/com/example/herohub/data/local/MarvelDataBase.kt
+++ app/src/main/java/com/example/herohub/data/local/MarvelDataBase.kt
@@ -1,9 +1,7 @@
 package com.example.herohub.data.local
 
-import android.content.Context
 import androidx.room.AutoMigration
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.herohub.data.local.dao.MarvelDao
 
@@ -21,20 +19,5 @@ import com.example.herohub.data.local.dao.MarvelDao
 )
 abstract class MarvelDataBase : RoomDatabase() {
     abstract fun marvelDao(): MarvelDao
-
-    companion object {
-        private const val DATABASE_NAME = "MarvelDataBase"
-
-        @Volatile
-        private var instance: MarvelDataBase? = null
-
-        fun getInstance(context: Context): MarvelDataBase {
-            return instance ?: synchronized(this) { buildDatabase(context).also { instance = it } }
-        }
-
-        private fun buildDatabase(context: Context): MarvelDataBase {
-            return Room.databaseBuilder(context, MarvelDataBase::class.java, DATABASE_NAME).build()
-        }
-    }
 
 }
